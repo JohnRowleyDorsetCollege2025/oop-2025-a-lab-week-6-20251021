3e0125f37b415f6d72da28a8d3e22572cff9eab8
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-10-21 13:40:10 +0100

    Add project files.

---
+++ oop-2025-b-lab-week-6-20251021/Models/DictionaryDemoDriver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2025_b_lab_week_6_20251021.Models
+{
+    public static class DictionaryDemoDriver
+    {
+        public static void Run()
+        {
+            CapitalCities();
+            FruitCounts();
+        }
+
+        public static void CapitalCities()
+        {
+            Dictionary<string, string> countryCapitals = new Dictionary<string, string>
+        {
+            { "FRANCE", "PARIS" },
+            { "IRELAND", "DUBLIN" },
+            { "UK", "LONDON" }
+        };
+
+            foreach (var entry in countryCapitals)
+            {
+                Console.WriteLine($"{entry.Key} -> {entry.Value}");
+            }
+        }
+
+        public static void FruitCounts()
+        {
+            Dictionary<string, int> fruitBasket = new Dictionary<string, int>
+        {
+            { "apple", 2 },
+            { "orange", 3 }
+        };
+
+            // Incoming list of fruits (e.g., someone adds these to the basket)
+            List<string> incomingFruits = new List<string> { "apple", "banana", "apple", "orange", "banana", "kiwi" };
+
+            // Update the dictionary counts
+            foreach (string fruit in incomingFruits)
+            {
+                if (fruitBasket.ContainsKey(fruit))
+                {
+                    fruitBasket[fruit]++;
+                }
+                else
+                {
+                    fruitBasket[fruit] = 1;
+                }
+            }
+
+            // Display updated fruit counts
+            foreach (var kvp in fruitBasket)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+        }
+    }
+}
---
+++ oop-2025-b-lab-week-6-20251021/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using oop_2025_b_lab_week_6_20251021.Models;
+
+Console.WriteLine("Loops and Dictionaries");
+
+
+LoopDemoDriver.Run();
+
+SimpleCheckoutDriver.Run();
+
+ComplexCheckoutDriver.Run();
+
+DictionaryDemoDriver.Run();
